7f16980628ca8872096d34faeb9d02fe401f45d4
Tarun Menon <tarunmenon95@gmail.com>
2023-02-15 11:20:19 +1100

    Added cognito return

--- ext/cfndsl/cognito.rb
+++ ext/cfndsl/cognito.rb
@@ -1,10 +1,13 @@
 
-def test_shared_function(alb_config={})
-  policies = []
-  print alb_config
-  alb_config.each do |name,policy|
-    print name
-    print policy
-  end
-  return 0
+def cognito_alb(user_pool_arn, user_pool_client_id, user_pool_domain)
+   #Required, create with the other component 
+  # -UserPoolArn
+  # -UserPoolClientId
+  # -UserPoolDomain
+  return { Type: "authenticate-cognito", 
+           AuthenticateCognitoConfig: {
+            UserPoolArn: user_pool_arn,
+            UserPoolClientId: user_pool_client_id,
+            UserPoolDomain: user_pool_domain
+         } }
 end
